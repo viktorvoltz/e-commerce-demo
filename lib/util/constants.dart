2c44bba14a88c64395370a1efdd96633c2f82d4c
Victor C Chinyeaka <vctrchinyeaka@gmail.com>
2024-07-28 00:53:56 +0100

    dev: consts

--- lib/util/constants.dart
+++ lib/util/constants.dart
@@ -1,6 +1,6 @@
-
 import 'package:flutter/material.dart';
 
 class ColorConstants{
   static const systemBlue = Color.fromARGB(255, 12, 84, 190);
+  static const systemScaffoldColor = Color(0xFFF5F9FD);
 }
