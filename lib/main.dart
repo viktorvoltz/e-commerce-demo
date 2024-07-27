f54061f16baf5e7a644066fd0e1e23f44f178209
Victor C Chinyeaka <vctrchinyeaka@gmail.com>
2024-07-28 00:51:40 +0100

    dev: themeing

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,12 @@
 import 'package:ecommerce/pages/login.dart';
+import 'package:ecommerce/pages/product_page.dart';
 import 'package:ecommerce/pages/signup.dart';
 import 'package:ecommerce/provider/auth_provider.dart';
 import 'package:ecommerce/provider/product_provider.dart';
 import 'package:ecommerce/provider/user_provider.dart';
 import 'package:ecommerce/services/remote_config.dart';
+import 'package:ecommerce/util/adaptive_spacing.dart';
+import 'package:ecommerce/util/constants.dart';
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 import 'package:firebase_core/firebase_core.dart';
@@ -26,6 +29,7 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    AdaptiveSpacing.init(context);
     return MultiProvider(
       providers: [
         ChangeNotifierProvider(
@@ -39,10 +43,15 @@ class MyApp extends StatelessWidget {
         ),
       ],
       child: MaterialApp(
+        theme: ThemeData(
+          fontFamily: 'Poppins',
+          scaffoldBackgroundColor: ColorConstants.systemScaffoldColor
+        ),
         initialRoute: '/',
         routes: <String, WidgetBuilder>{
           '/': (BuildContext context) => SignupScreen(),
           '/login': (BuildContext context) => LoginPage(),
+          '/product':(BuildContext context) => const ProductPage(),
         },
       ),
     );
