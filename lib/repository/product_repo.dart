4fa0f8d92ed8ade655e42bc0831adb713a2d07d9
Victor C Chinyeaka <vctrchinyeaka@gmail.com>
2024-07-27 23:11:13 +0100

    dev: repo

---
+++ lib/repository/product_repo.dart
@@ -0,0 +1,21 @@
+import 'dart:convert';
+import 'package:ecommerce/model/product_model.dart';
+import 'package:http/http.dart' as http;
+
+class ProductService {
+  Future<List<Product>> fetchProducts() async {
+    try {
+      final response = await http.get(Uri.parse('https://dummyjson.com/products'));
+
+      if (response.statusCode == 200) {
+        Map<String, dynamic> data = json.decode(response.body);
+        List<dynamic> productsData = data['products'];
+        return productsData.map((item) => Product.fromMap(item)).toList();
+      } else {
+        throw Exception('Failed to load products');
+      }
+    } catch (e) {
+      throw Exception('Error fetching products: $e');
+    }
+  }
+}
